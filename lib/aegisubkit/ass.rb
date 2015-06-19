dd3411794b6ef8caac234f0f8429a36c731ed7b0
Yuan He <lendage@gmail.com>
2015-06-19 16:24:01 +0800

    Add methods and return subtitle informations

--- lib/aegisubkit/ass.rb
+++ lib/aegisubkit/ass.rb
@@ -3,49 +3,76 @@ require 'treetop'
 module Aegisubkit
   module ASS
     class Subtitle < Treetop::Runtime::SyntaxNode
+      def build
+        subtitle = {}
+        subtitle[:events] = event_section.events.build
+        subtitle[:info] = info.build
+        subtitle[:audio_metadata] = audio_metadata.build
+        subtitle
+      end
     end
 
     class ScriptInfo < Treetop::Runtime::SyntaxNode
+      def build
+        elements.map do |line|
+          line.text_value
+        end.join
+      end
     end
 
     class AudioMetaData < Treetop::Runtime::SyntaxNode
+      def build
+        elements.map do |line|
+          next if line.text_value =~ /\[Aegisub Project Garbage\]/
+          next if line.text_value == '/n'
+          ary = line.text_value.split(/\n/).map do |x|
+            a = x.split(':')
+            a[0] = a.first.to_s.downcase.sub(/\s/, '_')
+            a.map(&:strip)
+          end.flatten
+          h = Hash[*ary]
+          h unless h.empty?
+        end.compact.first
+      end
     end
 
     class V4PStyles < Treetop::Runtime::SyntaxNode
-
+      def build
+        elements.map { |line| line.text_value }.join
+      end
     end
 
     class Events < Treetop::Runtime::SyntaxNode
       def build
         elements.map do |line|
-          subtitle = {}
+          events = {}
 
           fields = line.text_value.split(',')
 
-          subtitle[:id] = elements.index(line) + 1
-          subtitle[:start] = SSA.parse_timecode(fields[1])
-          subtitle[:end] = SSA.parse_timecode(fields[2])
-          subtitle[:lines] = fields[9..-1].join.gsub('\N', "\n")
+          events[:id] = elements.index(line) + 1
+          # subtitle[:start] = SSA.parse_timecode(fields[1])
+          # subtitle[:end] = SSA.parse_timecode(fields[2])
+          events[:lines] = fields[9..-1].join.sub!(/\N|\n/, '')
 
-          subtitle
+          events
         end
       end
     end
 
-    def self.import(string)
+    def self.import(file_path)
       Treetop.load(File.join(__dir__, 'ass'))
       parser = ASSParser.new
+      string = File.read(file_path, encoding: 'bom|utf-8')
       syntax_tree = parser.parse(string)
-
       if syntax_tree
-        return syntax_tree.build
+        syntax_tree.build
       else
         failure = "failure_index #{parser.failure_index}\n"
         failure += "failure_line #{parser.failure_line}\n"
         failure += "failure_column #{parser.failure_column}\n"
         failure += "failure_reason #{parser.failure_reason}\n"
 
-        raise failure
+        fail failure
       end
     end
   end
--- spec/aegisubkit/ass_spec.rb
+++ spec/aegisubkit/ass_spec.rb
@@ -1,20 +1,27 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe Aegisubkit::ASS do
   before do
-    # binding.pry
     Treetop.load File.expand_path('../../../lib/aegisubkit/ass', __FILE__)
-    @content = File.read File.expand_path('../../fixtures/example.ass', __FILE__)
+    @file_path = File.expand_path('../../fixtures/example.ass', __FILE__)
+    @result = Aegisubkit::ASS.import(@file_path)
   end
 
-  it 'should run this' do
-    parser = ASSParser.new
-    r = parser.parse @content
-    puts @content
-    if !r
-      puts parser.failure_reason
-      puts parser.failure_line
-      puts parser.failure_column
-    end
+  it 'should parser ass format file success' do
+    expect(@result).not_to be_nil
+  end
+
+  it 'should contains script infos' do
+    expect(@result[:info]).not_to be_nil
+  end
+
+  it 'should have events' do
+    expect(@result[:events]).not_to be_nil
+    expect(@result[:events].is_a?(Array)).to be_truthy
+  end
+
+  it 'should have audio metadata' do
+    expect(@result[:audio_metadata]).not_to be_nil
   end
 end
